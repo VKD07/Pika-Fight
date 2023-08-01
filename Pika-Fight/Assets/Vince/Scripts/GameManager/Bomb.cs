2c02fddb0fed45e9d72239d14c0fe85faf5ca6fd
Vince Delgado <116175769+VKD07@users.noreply.github.com>
2023-08-01 04:51:22 +0400

    Sticky Bomb Game Mode Added

--- Pika-Fight/Assets/Vince/Scripts/GameManager/Bomb.cs
+++ Pika-Fight/Assets/Vince/Scripts/GameManager/Bomb.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Bomb : MonoBehaviour
 {
     [SerializeField] GameObject []playersInScene;
     [SerializeField] string positionName;
     [SerializeField] float bombDuration = 20f;
-    int randomPlayer;
-    GameObject playerCarrier;
+    [SerializeField] GameObject explosionVfx;
+    float currentTime;
+    [SerializeField]int randomPlayer;
+    [SerializeField] GameObject playerCarrier;
     bool transfered;
-    private void Start()
+    
+    private void OnEnable()
     {
         playersInScene = GameObject.FindGameObjectsWithTag("Player");
         Randomize();
@@ -27,7 +31,7 @@ public class Bomb : MonoBehaviour
 
     void SetPosToPlayer()
     {
-        if(playersInScene.Length > 0)
+        if(playersInScene.Length > 0 && playerCarrier != null && playerCarrier.GetComponent<HealthBar>().healthValue > 0)
         {
             transform.position = playerCarrier.transform.Find("root/pelvis/spine_01/"+positionName).position;
         }
@@ -35,12 +39,33 @@ public class Bomb : MonoBehaviour
 
     void StartBombCountDown()
     {
-
+        if(playerCarrier != null)
+        {
+            if(currentTime < bombDuration)
+            {
+                currentTime += Time.deltaTime;
+            }
+            else
+            {
+                ExplosionVfx();
+                playerCarrier.GetComponent<ReceiveDamage>().GetDamage(100);
+                playerCarrier.GetComponent<PlayerConfigBridge>().PlayerConfig.PlayerIsDead = true;
+                Randomize();
+                playerCarrier = playersInScene[randomPlayer];
+                currentTime = 0;
+            }
+        }
     }
 
     void Randomize()
     {
         randomPlayer = Random.Range(0, playersInScene.Length);
+    }
+
+    void ExplosionVfx()
+    {
+        GameObject explosion = Instantiate(explosionVfx,transform.position, Quaternion.identity);
+        Destroy(explosion,2f);
     }
 
     private void OnTriggerEnter(Collider other)
---
+++ Pika-Fight/Assets/Vince/Scripts/GameManager/ReleaseTheBomb.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ReleaseTheBomb : MonoBehaviour
+{
+    [SerializeField] UnityEvent OnEnableScript;
+
+    private void OnEnable()
+    {
+        OnEnableScript.Invoke();
+    }
+}
