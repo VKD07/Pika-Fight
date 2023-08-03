6d2c1803575a04ae5e1ed6621f98c81703242132
Vince Delgado <116175769+VKD07@users.noreply.github.com>
2023-08-04 01:19:37 +0400

    Bug Fixes And Addons
    + Fixed Data resetting
    + Lowered Melee dash from 30 to 12
    + Fixed Player GOing outside the walls
    + Added barriers to fiery land

--- Pika-Fight/Assets/Vince/Scripts/Scriptable Object/PlayerConfig.cs
+++ Pika-Fight/Assets/Vince/Scripts/Scriptable Object/PlayerConfig.cs
@@ -21,15 +21,18 @@ public class PlayerConfig : ScriptableObject
 
     public PlayerControls Player_Controls
     {
-        set { playerControl = value; }
+        set { 
+            playerControl = value;
+            ForceSerialization();
+        }
         get { return playerControl; }
     }
 
     public Sprite CharacterSprite { get => characterSprite; set => characterSprite = value; }
 
-    public Color PlayerColor {get => playerColor;}
+    public Color PlayerColor { get => playerColor; }
 
-    public string CharacterName { get => characterName; set => characterName = value; } 
+    public string CharacterName { get => characterName; set => characterName = value; }
 
     public int PlayerScore { get => playerOverallScore; set => playerOverallScore = value; }
 
@@ -51,5 +54,12 @@ public class PlayerConfig : ScriptableObject
     private void OnDisable()
     {
         playerControl = null;
+    }
+
+    void ForceSerialization()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorUtility.SetDirty(this);
+#endif
     }
 }
