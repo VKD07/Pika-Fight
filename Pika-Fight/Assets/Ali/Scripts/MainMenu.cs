0ebc82846696a35172aafb9ac6c79eb4691f4a48
Aly Sharaf <ali.charaf59@gmail.com>
2023-08-06 14:28:17 +0400

    New Sprites for main menu, buttons move on click, new click sound, and can change scene number from main menu inspector

--- Pika-Fight/Assets/Ali/Scripts/MainMenu.cs
+++ Pika-Fight/Assets/Ali/Scripts/MainMenu.cs
@@ -5,17 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public int NumberSceneToLoad;
+
+    IEnumerator PlayGame1() 
+    {
+        yield return new WaitForSeconds(1.0f);
     
+        SceneManager.LoadScene(NumberSceneToLoad);
+    }
+
+    //IEnumerator QuitGame1() 
+    //{
+      //  yield return new WaitForSeconds(1.0f);
+    
+      //  Application.Quit();
+    //}
+    
+
     public void PlayGame ()
     {
-        
-        SceneManager.LoadScene("ShrinkLevel1"); // or to use scene numbers remoive brackets and type the number (also drag scene into build settings)
-
+        StartCoroutine(PlayGame1());
+     
+       //SceneManager.LoadScene(NumberSceneToLoad);
+       // SceneManager.LoadScene("ShrinkLevel1"); // or to use scene numbers remoive brackets and type the number (also drag scene into build settings)
     }
+
 
     public void QuitGame ()
     {
         Debug.Log("quit test");
         Application.Quit();
+       // StartCoroutine(Application.QuitGame1());
     }
 }
